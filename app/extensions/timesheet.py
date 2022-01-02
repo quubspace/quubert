94ff74a098946186b6b50e4811b6503813c0cf43
Daniil Rose <daniil.rose@member.fsf.org>
2022-01-02 13:46:19 -0500

    Add hours querying and adding

--- app/extensions/timesheet.py
+++ app/extensions/timesheet.py
@@ -1,8 +1,9 @@
 import time
-
 import hikari
 import lightbulb
 import pandas as pd
+
+from datetime import datetime
 from app.hours import Hours
 from app.utils.helpers import check_registration
 from hikari import Embed
@@ -28,14 +29,46 @@ async def hours(ctx: lightbulb.Context) -> None:
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def query(ctx: lightbulb.Context) -> None:
     hours = await Hours.load(user_id=ctx.author.id)
-    await ctx.respond(f"You have worked {hours.quantity} hours.")
+    quantity = sum([x.quantity for x in hours]) if hours else "no"
+
+    await ctx.respond(f"You have worked {quantity} hours this week.")
 
 
 @hours.child
+@lightbulb.option(
+    name="quantity", description="How many hours you worked.", type=int, required=True
+)
+@lightbulb.option(
+    name="date",
+    description="What day you worked on. In the format: YYYY-MM-DD",
+    type=str,
+    required=True,
+)
+@lightbulb.option(
+    name="description",
+    description="What you did. This is optional, but highly recommended.",
+    type=str,
+    required=False,
+)
 @lightbulb.command(name="add", description="Add hours to your timesheet.")
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def add(ctx: lightbulb.Context) -> None:
-    await ctx.respond("invoked hours add")
+    try:
+        date = datetime.strptime(ctx.options.date, "%Y-%m-%d")
+    except ValueError:
+        await ctx.respond("Please enter a date in the valid format: YYYY-MM-DD!")
+        return
+
+    hours = await Hours.add(
+        user_id=ctx.author.id,
+        quantity=ctx.options.quantity,
+        date=date,
+        description=ctx.options.description,
+    )
+
+    await ctx.respond(
+        f'Added {ctx.options.quantity} hours for {date.strftime("%Y-%m-%d")}: "{ctx.options.description}"'
+    )
 
 
 def load(bot) -> None:
--- app/hours.py
+++ app/hours.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import List, Optional, Tuple, Union
 
 from app import bot
@@ -22,25 +22,43 @@ class Hours:
         quantity: int = 0,
         description: str = None,
         date: datetime = None,
-        hours_obj: HoursModel = None,
-        db_object: bool = False,
-    ) -> Union["Hours", Tuple["Hours", HoursModel]]:
-        """Load an hours object through user_id or the hours_obj database objects.
-        If db_object flag is enabled, the method will also return the db_object."""
-        if hours_obj:
-            hours_db = hours_obj
-        else:
-            try:
-                hours_db = bot.hours_data[user_id]
-            except KeyError:
-                bot.hours_data[user_id] = await HoursModel.create(
-                    id=user_id,
-                    quantity=quantity,
-                    description=description,
-                    date=date,
-                )
-                hours_db = bot.hours_data[user_id]
-        hours = cls(
+    ) -> List["Hours"]:
+        """Load a list of hours objects from the last week through user_id."""
+        hours_db = bot.hours_data[user_id]
+
+        all_hours = (
+            await HoursModel.query.where(HoursModel.user_id == user_id)
+            .where(HoursModel.date <= datetime.now())
+            .where(HoursModel.date >= (datetime.now() - timedelta(days=7)))
+            .gino.all()
+        )
+
+        return [
+            cls(
+                id=hours_db.id,
+                user_id=hours_db.user_id,
+                quantity=hours_db.quantity,
+                description=hours_db.description,
+                date=hours_db.date,
+                db_object=bot.hours_data[user_id],
+            )
+            for hours_db in all_hours
+        ]
+
+    @classmethod
+    async def add(
+        cls, user_id: int, quantity: int, date: datetime, description: str = None
+    ) -> "Hours":
+        """Add hours to database, and return Hours object."""
+        bot.hours_data[user_id] = await HoursModel.create(
+            user_id=user_id,
+            quantity=quantity,
+            description=description,
+            date=date,
+        )
+        hours_db = bot.hours_data[user_id]
+
+        return cls(
             id=hours_db.id,
             user_id=hours_db.user_id,
             quantity=hours_db.quantity,
@@ -48,7 +66,3 @@ class Hours:
             date=hours_db.date,
             db_object=bot.hours_data[user_id],
         )
-        if db_object:
-            return hours, hours_db
-        else:
-            return hours
